e119740a137aec439645cfcb1b79ba0eb6e514e7
Gerodotry <Gerodotry@users.noreply.github.com>
2023-05-20 16:48:09 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="Gerodotry"
+original_name="hell"
+original_urlname="hell"
+original_description="Awesome hell created by Gerodotry"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv hell $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
